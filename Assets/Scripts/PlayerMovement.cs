217eb94f39b56bdbecf46eea839825b05e64bba9
yargee <yargee@bk.ru>
2021-01-25 12:04:09 +0300

    правки
    изменил скрипт PlayersMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,26 +14,10 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            Move(Vector2.up);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            Move(Vector2.down);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            Move(Vector2.left);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            Move(Vector2.right);
-        }
-    }
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+        var direction = new Vector3(horizontalMove, verticalMove, 0);
 
-    public void Move(Vector3 direction)
-    {
         transform.Translate(direction * _speed * Time.deltaTime);
     }
 
